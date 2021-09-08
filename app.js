45b9e76ed23b6d1c5fd74299d89a687f9d7ddae8
nottohave <nottohave@gmail.com>
2021-09-07 22:41:55 -0700

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 // practice fetch function
-// fetch('https://reqres.in/api/users', {
+
+// fetch('https://api.github.com/user', {
 //     method: 'POST',
 //     headers: {
 //         'Content-Type': 'application/json'
 //     },
 //     body: JSON.stringify({
-//         name: 'User 1'
+//         name: 'octocat'
 //     })
 // }).then(res => {
 //     return res.json()
 //     })
 //     .then(data => console.log(data))
 //     .catch(error => console.log('ERROR'))
+
+// practice callback and learning promise
+console.log("Start");
+
+function loginUser(email, password) {
+    setTimeout(() => {
+        console.log("We have the data.");
+        return({ userEmail: email });
+    }, 3000);
+}
+
+const user = loginUser('something@gmail.com', '123456')
+console.log("Finish");
