89a146e1ad8e9f7c23aedc5f2d9bbddcdd802f21
nottohave <nottohave@gmail.com>
2021-11-02 14:50:45 -0700

    app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var form = document.getElementById("myForm");
+
+// user profile picture and title
 var profilePicture = document.querySelector(".info__ProfilePic");
 var infoTitleH2 = document.querySelector(".info__title");
-var month = [
-    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
-    ];
+
+// user paragraph
+var infoUser = document.querySelector(".info__user");
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
@@ -17,7 +19,10 @@ form.addEventListener("submit", function(e){
     .then((data) => {
         console.log(data);
 
-        // var now = new Date((${data.created_at}).tostring());
+        // Date format
+        let dt = new Date(data.created_at).toLocaleDateString(undefined,{dateStyle:'medium'});
+
+        // User picture and title information
         profilePicture.innerHTML = 
         `
             <a target="_blank" href="https://www.github.com/${originalName}"> <img src="${data.avatar_url}"/>
@@ -25,8 +30,14 @@ form.addEventListener("submit", function(e){
         infoTitleH2.innerHTML = 
         `<h2>${data.name}</h2>` +
         `<a href="#">@${data.login}</a>` +
-        `<p>Joined ${data.created_at}</p>`;
+        `<p>Joined  `  + dt + `</p>`;
         
+        // User information description
+        if (data.bio == null) {
+            infoUser.innerHTML = `<p>This profile has no bio</p>`
+        } else {
+            infoUser.innherHTML = `<p>${data.bio}</p>`
+        }
     })
 
 })
