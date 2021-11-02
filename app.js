e3126a55645a87330ee28c6ca784604e5d584b92
nottohave <nottohave@gmail.com>
2021-11-01 21:38:40 -0700

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var form = document.getElementById("myForm");
 var profilePicture = document.querySelector(".info__ProfilePic");
 var infoTitleH2 = document.querySelector(".info__title");
+var month = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    ];
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
@@ -14,6 +17,7 @@ form.addEventListener("submit", function(e){
     .then((data) => {
         console.log(data);
 
+        // var now = new Date((${data.created_at}).tostring());
         profilePicture.innerHTML = 
         `
             <a target="_blank" href="https://www.github.com/${originalName}"> <img src="${data.avatar_url}"/>
@@ -21,7 +25,7 @@ form.addEventListener("submit", function(e){
         infoTitleH2.innerHTML = 
         `<h2>${data.name}</h2>` +
         `<a href="#">@${data.login}</a>` +
-        `<p>Joined</p>`;
+        `<p>Joined ${data.created_at}</p>`;
         
     })
 
